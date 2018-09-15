313644e06c7969f30bad75349ac741ca475f5603
jun <sangjun.bak99@gmail.com>
2018-09-15 09:46:20 -0400

    Set up more boiler plate

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyBu7z_n8OuItZsA0n4jUEVUNzCQsBAI9nQ";
+export const FIREBASE_AUTH_DOMAIN = "calorie-3dfee.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https =//calorie-3dfee.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "calorie-3dfee";
+export const FIREBASE_STORAGE_BUCKET = "calorie-3dfee.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "555819322402";
+
+export const FACEBOOK_APP_ID = "665446757120957";
