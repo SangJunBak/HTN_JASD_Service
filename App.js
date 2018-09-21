334822cfead8d9c8179e118b05b52d75fba47ce7
jun <sangjun.bak99@gmail.com>
2018-09-20 23:56:54 -0400

    Updated App.js

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-// import { Font, AppLoading } from 'expo';
 
 import Router from './app/config/routes'
 import store from './app/modules/redux/store';
 
-// function cacheFonts(fonts) {
-//     return fonts.map(font => Font.loadAsync(font));
-// }
-
-console.disableYellowBox= true;
+// console.disableYellowBox= true;
 
 export default class App extends Component {
     constructor() {
@@ -19,28 +14,7 @@ export default class App extends Component {
         }
     }
 
-    // async _loadAssetsAsync() {
-    //     const fontAssets = cacheFonts([
-    //         {RobotoExtraBold: require('./app/assets/fonts/Roboto-Black.ttf')},
-    //         {RobotoBold: require('./app/assets/fonts/Roboto-Bold.ttf')},
-    //         {RobotoMedium: require('./app/assets/fonts/Roboto-Medium.ttf')},
-    //         {RobotoRegular: require('./app/assets/fonts/Roboto-Regular.ttf')},
-    //         {RobotoLight: require('./app/assets/fonts/Roboto-Light.ttf')}
-    //     ]);
-    //
-    //     await Promise.all([...fontAssets]);
-    // }
-
     render() {
-        // if (!this.state.isReady) {
-        //     return (
-        //         <AppLoading
-        //             startAsync={this._loadAssetsAsync}
-        //             onFinish={() => this.setState({isReady: true})}
-        //             onError={console.warn}
-        //         />
-        //     );
-        // }
         
         return (
             <Provider store={store}>
