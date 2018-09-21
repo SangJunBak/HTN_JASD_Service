ec1f98dec2a16ec3eac94aeeb4399b9fee13c998
jun <sangjun.bak99@gmail.com>
2018-09-20 23:54:14 -0400

    Updated constants.js

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,8 +1,8 @@
-export const FIREBASE_API_KEY = "AIzaSyCAQvq6Y_Z3Ch_SZxzSvVQA68B2JjEdz1o";
-export const FIREBASE_AUTH_DOMAIN = "hackthenorth-jasd.firebaseapp.com";
-export const FIREBASE_DATABASE_URL = "https://hackthenorth-jasd.firebaseio.com";
-export const FIREBASE_PROJECT_ID = "hackthenorth-jasd";
-export const FIREBASE_STORAGE_BUCKET = "hackthenorth-jasd.appspot.com";
-export const FIREBASE_MESSAGING_SENDER_ID = "350901889105";
+export const FIREBASE_API_KEY = "";
+export const FIREBASE_AUTH_DOMAIN = "";
+export const FIREBASE_DATABASE_URL = "";
+export const FIREBASE_PROJECT_ID = "";
+export const FIREBASE_STORAGE_BUCKET = "";
+export const FIREBASE_MESSAGING_SENDER_ID = "";
 
-export const FACEBOOK_APP_ID = "266381110656153";
+export const FACEBOOK_APP_ID = "";
